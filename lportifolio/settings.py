031c1a22a3ca595147d11e6e1e987fa79921b1b0
schultzl <lkp.schultze@gmail.com>
2023-01-19 16:18:12 +0100

    Omittiing sensitive variables

--- lportifolio/settings.py
+++ lportifolio/settings.py
@@ -146,9 +146,9 @@ MEDIA_URL = "media/"
 MEDIA_ROOT = str(BASE_DIR) + "/static/media/images"
 
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
-EMAIL_HOST='smtp.gmail.com'
-EMAIL_PORT=587
-EMAIL_HOST_USER = "flowy.future@gmail.com"
-EMAIL_HOST_PASSWORD = ''  
-DEFAULT_FROM_EMAIL = "flowy.future@gmail.com"
-EMAIL_USE_TLS = True
+EMAIL_HOST=config("EMAIL_HOST")
+EMAIL_PORT=config("EMAIL_PORT")
+EMAIL_HOST_USER = config("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")
+DEFAULT_FROM_EMAIL = config("DEFAULT_FROM_EMAIL")
+EMAIL_USE_TLS = config("EMAIL_USE_TLS")
