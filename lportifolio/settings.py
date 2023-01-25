6d2f8f0d57a172a185b4409bdd08f28ab02edf4a
schultzl <lkp.schultze@gmail.com>
2023-01-25 10:25:04 +0100

    updated settings

--- lportifolio/settings.py
+++ lportifolio/settings.py
@@ -11,8 +11,13 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-from decouple import config
+#from decouple import config
+from decouple import Config, RepositoryEnv
 import os
+
+
+DOTENV_FILE = '.privenv'
+env_config = Config(RepositoryEnv(DOTENV_FILE))
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config("SECRET_KEY")
+SECRET_KEY = env_config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -146,9 +151,9 @@ MEDIA_URL = "media/"
 MEDIA_ROOT = str(BASE_DIR) + "/static/media/images"
 
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
-EMAIL_HOST=config("EMAIL_HOST")
-EMAIL_PORT=config("EMAIL_PORT")
-EMAIL_HOST_USER = config("EMAIL_HOST_USER")
-EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")
-DEFAULT_FROM_EMAIL = config("DEFAULT_FROM_EMAIL")
-EMAIL_USE_TLS = config("EMAIL_USE_TLS")
+EMAIL_HOST=env_config("EMAIL_HOST")
+EMAIL_PORT=env_config("EMAIL_PORT")
+EMAIL_HOST_USER = env_config("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = env_config("EMAIL_HOST_PASSWORD")
+DEFAULT_FROM_EMAIL = env_config("DEFAULT_FROM_EMAIL")
+EMAIL_USE_TLS = env_config("EMAIL_USE_TLS")
